c516b0d56d3497767335b2a1bce2bad9833cd070
marshallg02 <122751535+marshallg02@users.noreply.github.com>
2023-01-15 16:03:57 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,7 +6,31 @@ namespace hello_world_cshp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Solution (1): Hello, World!");
+
+            Console.WriteLine(" ");
+
+            string stg = "Hello, World!";
+
+            Console.WriteLine("Solution (2): " + stg);
+
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Enter a user's name: ");
+
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("The user's name is: " + userName);
+
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Ente the user's age: ");
+
+            //int age = Convert.ToInt32(Console .ReadLine());
+
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The user's age is: " + age);
         }
     }
 }
